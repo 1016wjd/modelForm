f9dbc787d5cdce0b8f91727912710706c1065148
KimHyunJung <hday10166@gamil.com>
2023-08-24 13:06:55 +0900

    create 기능 구현 1차

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
+
 # Create your views here.
 def index(request):
     articles = Article.objects.all()
@@ -18,3 +20,27 @@ def detail(request,id):
     }
 
     return render(request, 'detail.html', context)
+
+
+def create(request):
+    # 사용자가 입력한 데이터를 DB에 저장
+    if request.method =='POST':
+        form = ArticleForm(request.POST)
+
+            # 데이터가 잘 들어왔는지 한번 더 검증
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', id=article.id)
+
+
+    # 사용자가 데이터를 입력할 수 있도록 빈 종이를 리턴 (GET인 경우)
+    else:
+        form = ArticleForm()
+
+        context = {
+            'form': form,
+        }
+
+        return render(request, 'create.html', context)
+
+    
